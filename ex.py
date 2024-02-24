57639971666ed1e21ecbeee44b92f71c8f81aa5f
Muhammed <muhammedsva93@gmail.com>
2024-02-24 11:57:15 +0200

    ex

--- ex.py
+++ ex.py
@@ -8,18 +8,18 @@ header = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/5
 for j in range(1, 11):
     url = f"https://onlyballs.com.ua/kupit-myachi-futbolnye={j}"
 
-    response = session.get(url, headers=header)
-    soup = BeautifulSoup(response.text,"lxml")
-    print(soup)
+response = session.get(url, headers=header)
+soup = BeautifulSoup(response.text,"lxml")
+print(soup)
 
-    product-name = soup.find('div', class_="big_grid row form-row row-cols-1 row-cols-sm-2 row-cols-xl-3 row-cols-xxl-4")
+all_product = soup.find('div', class_="big_grid row form-row row-cols-1 row-cols-sm-2 row-cols-xl-3 row-cols-xxl-4")
 
-    products = product-name.find_all("div", class_="product-name")
+products = all_product.find_all("div", class_="product-name")
 
-    for elem in products:
-        price = elem.find("p", class_="card-text").text
-        price = price[price.find(":")+1:price.find("₽")]
-        title = elem.find("h3", class_="card-title").text.strip()
-        with open("ex.txt", "a", encoding="utf-8") as file:
-            file.write(f"{title}\n")
-        print(price)
+for elem in products:
+    price = elem.find("p", class_="card-text").text
+    price = price[price.find(":")+1:price.find("₽")]
+    title = elem.find("h3", class_="card-title").text.strip()
+    with open("ex.txt", "a", encoding="utf-8") as file:
+        file.write(f"{title}\n")
+    print(price)
